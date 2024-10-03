3b781918d0fdd4022753bc8bc60d005bbcd49c7c
amamyyyyy <amemiyatech@gmail.com>
2024-10-03 19:37:20 +0900

    アプリの雛形を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  root to: "recipes#index"
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
